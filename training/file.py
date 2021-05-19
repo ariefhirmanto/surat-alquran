1cb30f3523c2ec58641bd555e519e15ea0ec7d15
Arief Hirmanto <ariefhirmanto7@gmail.com>
2021-05-19 21:26:25 +0700

    update metadata

--- training/file.py
+++ training/file.py
@@ -1,12 +1,23 @@
 import os
 import shutil
-path1 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat12'
-path2 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat13'
-path3 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat14'
-path4 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat15'
-path5 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat16'
-path6 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat17'
-path7 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat18'
+path1 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat1'
+path2 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat2'
+path3 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat3'
+path4 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat4'
+path5 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat5'
+path6 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat6'
+path7 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat7'
+path8 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat8'
+path9 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat9'
+path10 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat10'
+path11 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat11'
+path12 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat12'
+path13 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat13'
+path14 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat14'
+path15 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat15'
+path16 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat16'
+path17 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat17'
+path18 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat18'
 # path8 = '/Users/IMAN/Documents/Machine Learning/DSKC/Dataset fix/training/surat11'
 files1 = os.listdir(path1)
 files2 = os.listdir(path2)
@@ -15,6 +26,17 @@ files4 = os.listdir(path4)
 files5 = os.listdir(path5)
 files6 = os.listdir(path6)
 files7 = os.listdir(path7)
+files8 = os.listdir(path8)
+files9 = os.listdir(path9)
+files10 = os.listdir(path10)
+files11 = os.listdir(path11)
+files12 = os.listdir(path12)
+files13 = os.listdir(path13)
+files14 = os.listdir(path14)
+files15 = os.listdir(path15)
+files16 = os.listdir(path16)
+files17 = os.listdir(path17)
+files18 = os.listdir(path18)
 # files8 = os.listdir(path8)
 for file in files1:
     print(file)
@@ -29,6 +51,28 @@ for file in files5:
 for file in files6:
     print(file)
 for file in files7:
+    print(file)
+for file in files8:
+    print(file)
+for file in files9:
+    print(file)
+for file in files10:
+    print(file)
+for file in files11:
+    print(file)
+for file in files12:
+    print(file)
+for file in files13:
+    print(file)
+for file in files14:
+    print(file)
+for file in files15:
+    print(file)
+for file in files16:
+    print(file)
+for file in files17:
+    print(file)
+for file in files18:
     print(file)
 # for file in files8:
 #     print(file)
